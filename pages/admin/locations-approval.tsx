6568f098cb373562c0304c2b6c2732a169c4a5cc
ibanesve <ibanesve@yahoo.com>
2025-07-11 10:28:44 -0700

    Update locations-approval.tsx

--- pages/admin/locations-approval.tsx
+++ pages/admin/locations-approval.tsx
@@ -3,11 +3,8 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-// Set up the Supabase client
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+import { supabase } from "../../lib/supabase";
+
 
 export default function LocationsApproval() {
   const [locations, setLocations] = useState([]);
